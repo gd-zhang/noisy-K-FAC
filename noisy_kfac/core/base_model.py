c3fc796dee0f681fc55b29f32db878fac60e8d8b
Steven H. Wang <wang.steven.h@gmail.com>
2018-09-27 20:31:00 -0700

    Merge some shared code between {IRD,}Model

--- noisy_kfac/core/base_model.py
+++ noisy_kfac/core/base_model.py
@@ -7,6 +7,15 @@ class BaseModel:
         # init the global step
         self.init_global_step()
 
+    @property
+    def trainable_variables(self):
+        # note: we don't train the params of BN
+        vars = []
+        for var in tf.trainable_variables():
+            if "train_w_" in var.name:
+                vars.append(var)
+        return vars
+
     # save function that saves the checkpoint in the path defined in the config file
     def save(self, sess):
         print("Saving model...")
@@ -26,10 +35,33 @@ class BaseModel:
         # DON'T forget to add the global step tensor to the tensorflow trainer
         self.global_step_tensor = tf.train.get_or_create_global_step()
 
+    def init_optim(self):
+        self.optim = opt.KFACOptimizer(
+            var_list=self.trainable_variables,
+            learning_rate=self.config.learning_rate,
+            cov_ema_decay=self.config.cov_ema_decay,
+            damping=self.config.damping,
+            layer_collection=self.layer_collection,
+            norm_constraint=tf.train.exponential_decay(self.config.kl_clip,
+                                                       self.global_step_tensor,
+                                                       390, 0.95,
+                                                       staircase=True),
+            momentum=self.config.momentum)
+
+        self.cov_update_op = self.optim.cov_update_op
+        self.inv_update_op = self.optim.inv_update_op
+
+        with tf.control_dependencies([self.inv_update_op]):
+            self.var_update_op = self.sampler.update(
+                    self.layer_collection.get_blocks())
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            self.train_op = self.optim.minimize(self.total_loss,
+                    global_step=self.global_step_tensor)
+
     def init_saver(self):
-        # just copy the following line in your child class
-        # self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
-        raise NotImplementedError
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
 
     def build_model(self):
         raise NotImplementedError
--- noisy_kfac/core/model.py
+++ noisy_kfac/core/model.py
@@ -70,27 +70,3 @@ class Model(BaseModel):
         coeff = self.config.kl / (self.n_data * self.config.eta)
         self.total_loss = self.loss + coeff * l2_loss
 
-    def init_optim(self):
-        self.optim = opt.KFACOptimizer(var_list=self.trainable_variables,
-                                       learning_rate=self.config.learning_rate,
-                                       cov_ema_decay=self.config.cov_ema_decay,
-                                       damping=self.config.damping,
-                                       layer_collection=self.layer_collection,
-                                       norm_constraint=tf.train.exponential_decay(self.config.kl_clip,
-                                                                                  self.global_step_tensor,
-                                                                                  390, 0.95, staircase=True),
-                                       momentum=self.config.momentum)
-
-        self.cov_update_op = self.optim.cov_update_op
-        self.inv_update_op = self.optim.inv_update_op
-
-        with tf.control_dependencies([self.inv_update_op]):
-            self.var_update_op = self.sampler.update(self.layer_collection.get_blocks())
-
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            self.train_op = self.optim.minimize(self.total_loss, global_step=self.global_step_tensor)
-
-    def init_saver(self):
-        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
-
